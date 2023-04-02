ba865ff3a6f6a38634d7c44f6914e6e08b987378
ks-young <katherine.s.young@kcl.ac.uk>
2023-04-02 20:30:16 +0100

    updates

--- scripts/functions/relative_risk_forest.R
+++ scripts/functions/relative_risk_forest.R
@@ -1,9 +1,9 @@
 # Function to create forest plots  using formatted data
 
 
-relrisk.forest <- function(data,plot_title,sub_title,pointsize=2,linesize=1,errorsize=1,
-                      axistext=10,axistitle=12,legtitle=12,stripsize=12,title_size=12,
-                      colourpal=forest.palette,lowlim=0.01,uplim=6,dodge=0.5)
+relrisk.forest <- function(data,plot_title,sub_title,pointsize=2,linesize=0.2,errorsize=0.5,
+                      axistext=10,axistitle=10,legtitle=12,stripsize=24,title_size=10,
+                      colourpal=forest.palette,lowlim=-1,uplim=6,dodge=0.5)
   
   {
   p = ggplot(data=data,
@@ -14,7 +14,8 @@ relrisk.forest <- function(data,plot_title,sub_title,pointsize=2,linesize=1,erro
                position = position_dodge(dodge)) +
     
     geom_hline(yintercept =1,
-               linetype=1,
+               linetype=2,
+               colour = "grey",
                size = linesize)  +
     
     xlab('Class') +
@@ -22,23 +23,23 @@ relrisk.forest <- function(data,plot_title,sub_title,pointsize=2,linesize=1,erro
     
     geom_errorbar(aes(ymin = lower,
                       ymax = upper),
-                  width = 1,
+                  width = 0.8,
                   position = position_dodge(dodge),
                   size = errorsize)  +
     
     #facet_wrap(. ~ Categories,
     #           strip.position="left",nrow=6, scales="free_y") +
-    
     facet_grid(. ~ class,  drop = TRUE) +
     
     scale_colour_manual(values=colourpal) +
-    scale_y_log10(limits = c(lowlim,uplim)) + # Log-scale
+    #scale_y_log10(limits = c(lowlim,uplim)) + # Log-scale
+    scale_y_continuous(limits = c(lowlim,uplim)) +
     theme(plot.title=element_text(face="bold",size=axistext,), #element_text(size=0,face="bold"),
-          axis.text=element_text(face="bold",size=axistext,),
+          axis.text=element_text(face="plain",size=axistext,),
           legend.text=element_text(size=title_size),
           legend.title=element_text(face="bold",size=legtitle,),
-          legend.position = "none",
-          axis.title.x=element_blank(), #element_text(size=axistitle,face="bold"),
+          #legend.position = "none",
+          #axis.title.x=element_blank(), #element_text(size=axistitle,face="bold"),
           axis.title.y=element_blank(),
           strip.text.y = element_text(angle=180,face="bold",size=stripsize),
           panel.background = element_blank(),
@@ -46,6 +47,7 @@ relrisk.forest <- function(data,plot_title,sub_title,pointsize=2,linesize=1,erro
           panel.border = element_rect(colour = "grey",fill = NA)) +
     # guides(color = guide_legend(reverse = TRUE)) +
 
+    
     ggtitle(plot_title,
             subtitle = sub_title) +
 
